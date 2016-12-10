b45720689e6324ab58653eca1cb5d930d6daf7b8
ぶらっく <smzy.black@outlook.com>
2016-12-11 01:20:39 +0900

    Add custom event counter name

--- XamarinDistributeSample/XamarinDistributeSample/Model/Member.cs
+++ XamarinDistributeSample/XamarinDistributeSample/Model/Member.cs
@@ -4,6 +4,7 @@
     {
         public string FullName { get; set; }
         public string UserId { get; set; }
+        public string UserCounterName { get; set; }
 
         public string Team { get; set; }
     }
